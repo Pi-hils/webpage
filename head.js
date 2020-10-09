0b2d68b4b55cd5b0e6b949b14894ecc1e90efaea
Hilda Amponsah <hilda.amp@hotmail.com>
2020-10-09 16:15:23 +0100

    website

---
+++ head.js
@@ -0,0 +1,69 @@
+var TxtType = function(el, toRotate, period) {
+  this.toRotate = toRotate;
+  this.el = el;
+  this.loopNum = 0;
+  this.period = parseInt(period, 10) || 2000;
+  this.txt = '';
+  this.tick();
+  this.isDeleting = false;
+};
+
+TxtType.prototype.tick = function() {
+  var i = this.loopNum % this.toRotate.length;
+  var fullTxt = this.toRotate[i];
+
+  if (this.isDeleting) {
+  this.txt = fullTxt.substring(0, this.txt.length - 1);
+  } else {
+  this.txt = fullTxt.substring(0, this.txt.length + 1);
+  }
+
+  this.el.innerHTML = '<span class="wrap">'+this.txt+'</span>';
+
+  var that = this;
+  var delta = 200 - Math.random() * 100;
+
+  if (this.isDeleting) { delta /= 2; }
+
+  if (!this.isDeleting && this.txt === fullTxt) {
+  delta = this.period;
+  this.isDeleting = true;
+  } else if (this.isDeleting && this.txt === '') {
+  this.isDeleting = false;
+  this.loopNum++;
+  delta = 500;
+  }
+
+  setTimeout(function() {
+  that.tick();
+  }, delta);
+};
+
+window.onload = function() {
+  var elements = document.getElementsByClassName('typewrite');
+  for (var i=0; i<elements.length; i++) {
+      var toRotate = elements[i].getAttribute('data-type');
+      var period = elements[i].getAttribute('data-period');
+      if (toRotate) {
+        new TxtType(elements[i], JSON.parse(toRotate), period);
+      }
+  }
+  // INJECT CSS
+  var css = document.createElement("style");
+  css.type = "text/css";
+  css.innerHTML = ".typewrite > .wrap { border-right: 0.08em solid #fff}";
+  document.body.appendChild(css);
+};
+
+// swipe at about me for more
+
+function swipe(){
+  var x=document.getElementById("me");
+  var y ="I am an ambitious, creative and self-motivated <strong>Full Stack Developer</strong> who enjoys solving complex problems and helping my team to succeed. As I take genuine pleasure in dissecting concepts to examine what works best and why it works or does not work, I decided to study at one of Europe's leading coding bootcamp, Makers Academy.<br><br/>"
+  var z = "An outgoing, avocado and chicken aficionado with excellent taste in good food and finding secret spots"
+  if(x.innerHTML === y){
+    x.innerHTML = z;
+  }else {
+    x.innerHTML = z;
+  }
+}
